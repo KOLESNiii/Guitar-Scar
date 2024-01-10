91d0c759d5aa4e8b3fa53375892061de3967482f
KOLESNiii <timkolesnichenko05@gmail.com>
2024-01-10 20:00:45 +0000

    Changed to switch statements

--- Assets/Scripts/EnemyTypeManager.cs
+++ Assets/Scripts/EnemyTypeManager.cs
@@ -27,81 +27,51 @@ public class EnemyTypeManager : MonoBehaviour
     //Returns the enemy type based on the int passed in
     public EnemyType getType(int Int)
     {
-        if (Int == 0)
+        switch (Int)
         {
-            return cmaj;
-        }
-        else if (Int == 1)
-        {
-            return efmaj;
-        }
-        else if (Int == 2)
-        {
-            return fmaj;
-        }
-        else if (Int == 3)
-        {
-            return dmaj;
-        }
-        else if (Int == 4)
-        {
-            return amaj;
-        }
-        else if (Int == 5)
-        {
-            return emaj;
-        }
-        else if (Int == 6)
-        {
-            return dfmaj;
-        }
-        else if (Int == 7)
-        {
-            return gmaj;
-        }
-        else
-        {
-            return all;
+            case 0:
+                return cmaj;
+            case 1:
+                return efmaj;
+            case 2:
+                return fmaj;
+            case 3:
+                return dmaj;
+            case 4:
+                return amaj;
+            case 5:
+                return emaj;
+            case 6:
+                return dfmaj;
+            case 7:
+                return gmaj;
+            default:
+                return all;
         }
     }
     //Returns a random enemy sprite based on the enemy type passed in
     public GameObject getEnemy(EnemyType type)
     {
-        if (type == cmaj)
+        switch (type)
         {
-            return cMajSprites[Random.Range(0, cMajSprites.Count)];
-        } 
-        else if (type == efmaj)
-        {
-            return efMajSprites[Random.Range(0, efMajSprites.Count)];
-        }
-        else if (type == fmaj)
-        {
-            return fMajSprites[Random.Range(0, fMajSprites.Count)];
-        }
-        else if (type == dmaj)
-        {
-            return dMajSprites[Random.Range(0, dMajSprites.Count)];
-        }
-        else if (type == amaj)
-        {
-            return aMajSprites[Random.Range(0, aMajSprites.Count)];
-        }
-        else if (type == emaj)
-        {
-            return eMajSprites[Random.Range(0, eMajSprites.Count)];
-        }
-        else if (type == dfmaj)
-        {
-            return dfMajSprites[Random.Range(0, dfMajSprites.Count)];
-        }
-        else if (type == gmaj)
-        {
-            return gMajSprites[Random.Range(0, gMajSprites.Count)];
-        }
-        else
-        {
-            return allSprites[Random.Range(0, allSprites.Count)];
+            case cmaj:
+                return cMajSprites[Random.Range(0, cMajSprites.Count)];
+            case efmaj:
+                return efMajSprites[Random.Range(0, efMajSprites.Count)];
+            case fmaj:
+                return fMajSprites[Random.Range(0, fMajSprites.Count)];
+            case dmaj:
+                return dMajSprites[Random.Range(0, dMajSprites.Count)];
+            case amaj:
+                return aMajSprites[Random.Range(0, aMajSprites.Count)];
+            case emaj:
+                return eMajSprites[Random.Range(0, eMajSprites.Count)];
+            case dfmaj:
+                return dfMajSprites[Random.Range(0, dfMajSprites.Count)];
+            case gmaj:
+                return gMajSprites[Random.Range(0, gMajSprites.Count)];
+            default:
+                return allSprites[Random.Range(0, allSprites.Count)];
         }
     }
 }
